dbbcadc9bae243c2fee1c7c5348297f69e8401b4
madgelackie <marionmlackie@gmail.com>
2022-01-07 06:36:56 +0000

    unit tests completed

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -11,14 +11,7 @@ describe('Calculator', () => {
     container = mount(<Calculator/>)
   })
 
-  it('should change running total on number enter', () => {
-    const button4 = container.find('#number4');
-    const runningTotal = container.find('#running-total');
-    button4.simulate('click');
-    expect(runningTotal.text()).toEqual('4');
-  })
-
-  it('should add two numbers together to give new running total', () => {
+  it('should add two numbers together', () => {
     const button1 = container.find('#number1');
     const button4 = container.find('#number4'); 
     const operatorAdd = container.find('#operator_add');
@@ -31,7 +24,7 @@ describe('Calculator', () => {
     expect(runningTotal.text()).toEqual('5');
   })
 
-it('should subtract one number from another to give new running total', () => {
+it('should subtract one number from another', () => {
     const button7 = container.find('#number7');
     const button4 = container.find('#number4'); 
     const operatorSubtract = container.find('#operator-subtract');
@@ -44,7 +37,7 @@ it('should subtract one number from another to give new running total', () => {
     expect(runningTotal.text()).toEqual('3');
 })
 
-it('should multiply one number by another to give new running total', () => {
+it('should multiply one number by another', () => {
   const button3 = container.find('#number3');
   const button5 = container.find('#number5'); 
   const operatorMultiply = container.find('#operator-multiply');
@@ -57,7 +50,7 @@ it('should multiply one number by another to give new running total', () => {
   expect(runningTotal.text()).toEqual('15');
 })
 
-it('should divide one number by another to give a new running total', () => {
+it('should divide one number by another', () => {
   const button2 = container.find('#number2');
   const button1 = container.find('#number1');
   const button7 = container.find('#number7'); 
@@ -80,6 +73,45 @@ it('should concatenate multiple number clicks', () => {
   button8.simulate('click');
   expect(runningTotal.text()).toEqual('888');
 
+})
+
+it('should be able to update the running total following chained operations', () => {
+  const button2 = container.find('#number2')
+  const button5 = container.find('#number5')
+  const button6 = container.find('#number6')
+  const operatorMultiply = container.find('#operator-multiply');
+  const operatorSubtract = container.find('#operator-subtract')
+  const runningTotal = container.find('#running-total');
+  const operatorEquals = container.find('#operator-equals');
+  button2.simulate('click');
+  operatorMultiply.simulate('click');
+  button5.simulate('click');
+  operatorSubtract.simulate('click');
+  button6.simulate('click');
+  operatorEquals.simulate('click');
+  expect(runningTotal.text()).toEqual('4');
+})
+
+it ('should clear running total without affecting calculation', () => {
+  const button5 = container.find("#number5");
+  const button2 = container.find("#number2");
+  const button3 = container.find("#number3");
+  const operatorMultiply = container.find('#operator-multiply');
+  const operatorAdd = container.find('#operator_add');
+  const operatorSubtract = container.find('#operator-subtract');
+  const clear = container.find('#clear');
+  const runningTotal = container.find('#running-total');
+  const operatorEquals = container.find('#operator-equals');
+  button5.simulate('click');
+  operatorMultiply.simulate('click');
+  button2.simulate('click');
+  operatorEquals.simulate('click');
+  operatorAdd.simulate('click');
+  clear.simulate('click');
+  operatorSubtract.simulate('click');
+  button3.simulate('click');
+  operatorEquals.simulate('click');
+  expect(runningTotal.text()).toEqual('7')
 })
 
 })
